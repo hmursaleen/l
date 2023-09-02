e63f0dd82bd9b0f813c296780461db5ccb0d556c
hmursaleen <habibulmursaleen@gmail.com>
2023-09-02 15:53:19 +0600

    try

--- shopit/settings.py
+++ shopit/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -150,13 +149,12 @@ EMAIL_HOST_PASSWORD = 'xbysulfpxxtpdujr'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
